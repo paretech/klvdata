0a07096010d744176ebd6437ee075e4430552d61
paretech <paretech@gmail.com>
2017-09-10 16:48:59 -0400

    First steps towards PyPI.

---
+++ setup.py
@@ -0,0 +1,81 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='klvdata',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.0',
+
+    description='A Python library for parsing MISB/STANAG 4609 Key Length Value (KLV) metadata.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/paretech/klvdata/',
+
+    # Author details
+    author='Matthew Pare',
+    author_email="paretech@gmail.com",
+    
+    # License details
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   1 - Planning
+        #   2 - Pre-Alpha
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 2 - Pre-Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+
+        # Specify related topics
+        'Topic :: Multimedia :: Video :: Conversion',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries',
+    ],
+
+    # What does your project relate to?
+    keywords='STANAG 4609, MISB, KLV, Metadata, Video',
+
+    packages=['klvdata'],
+    test_suite="tests",
+
+    python_requires='>=3.5',
+  
+)
+
