94bbf41cee092fd8383699a27df306217be83094
paretech <paretech@gmail.com>
2018-01-12 12:17:19 -0500

    Fixed typo.

--- klvdata/streamparser.py
+++ klvdata/streamparser.py
@@ -46,7 +46,7 @@ class StreamParser:
             return self.parsers[key](value)
         else:
             # Even if KLV is not known, make best effort to parse and preserve.
-            # Element is an abastract super class, do not create instances on Element.
+            # Element is an abstract super class, do not create instances on Element.
             return UnknownElement(key, value)
 
     @classmethod
