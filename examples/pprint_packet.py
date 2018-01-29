299368ff993f14e4356bfad1b967d165bdb39d11
Francisco Raga <All4Gis@users.noreply.github.com>
2018-01-29 09:24:56 +0100

    Correct indentation
    Correct indentation example packet

--- examples/pprint_packet.py
+++ examples/pprint_packet.py
@@ -3,6 +3,6 @@
 import klvdata
 
 if __name__ == "__main__":
-with open('./data/DynamicConstantMISMMSPacketData.bin', 'rb') as f:
-	for packet in klvdata.StreamParser(f):
-		packet.structure()
+	with open('./data/DynamicConstantMISMMSPacketData.bin', 'rb') as f:
+		for packet in klvdata.StreamParser(f):
+			packet.structure()
